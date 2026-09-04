c87169940a0fae08f872296a680ab4aa98bbf078
DimaKhaled <dkh3462@gmail.com>
2026-09-04 11:57:00 +0300

    implement authentication endpoints

---
+++ src/PlaylistApi.API/Controllers/AuthController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PlaylistApi.Application.DTOs.Auth;
+using PlaylistApi.Application.Interfaces.Services;
+
+namespace PlaylistApi.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+
+        [HttpPost("register")]
+        [AllowAnonymous]
+        public async Task<ActionResult<AuthResponse>> Register(RegisterRequest request)
+        {
+            var response = await _authService.RegisterAsync(request);
+            return Ok(response);
+        }
+
+
+
+        [HttpPost("login")]
+        [AllowAnonymous]
+        public async Task<ActionResult<AuthResponse>> Login(LoginRequest request)
+        {
+            var response = await _authService.LoginAsync(request);
+            return Ok(response);
+        }
+    }
+}
