cd31df9213141ec3bdd187bfa5ba5985f1c6a818
DimaKhaled <dkh3462@gmail.com>
2026-09-04 09:19:06 +0300

    configure infrastructure dependency injection

--- src/PlaylistApi.Infrastructure/Authentication/JwtTokenService.cs
+++ src/PlaylistApi.Infrastructure/Authentication/JwtTokenService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Options;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using PlaylistApi.Application.Interfaces.Authentication;
 using System.IdentityModel.Tokens.Jwt;
@@ -9,29 +10,34 @@ namespace PlaylistApi.Infrastructure.Authentication
 {
     public class JwtTokenService : IJwtTokenService
     {
-        private readonly JwtSettings _jwtSettings;
+        private readonly IConfiguration _configuration;
 
-        public JwtTokenService(IOptions<JwtSettings> options)
+        public JwtTokenService(IConfiguration configuration)
         {
-            _jwtSettings = options.Value;
+            _configuration = configuration;
         }
 
 
         public string GenerateToken(Guid userId, string email)
         {
+            var key = _configuration["Jwt:Key"]!;
+            var issuer = _configuration["Jwt:Issuer"]!;
+            var audience = _configuration["Jwt:Audience"]!;
+
+
             var claims = new[]
             {
                 new Claim(ClaimTypes.NameIdentifier, userId.ToString()),
                 new Claim(ClaimTypes.Email, email)
             };
 
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSettings.Key));
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
 
-            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
 
             var token = new JwtSecurityToken(
-                issuer: _jwtSettings.Issuer,
-                audience: _jwtSettings.Audience,
+                issuer: issuer,
+                audience: audience,
                 claims: claims,
                 expires: DateTime.Now.AddHours(1),
                 signingCredentials: credentials
---
+++ src/PlaylistApi.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using PlaylistApi.Application.Interfaces.Authentication;
+using PlaylistApi.Application.Interfaces.ExternalServices;
+using PlaylistApi.Application.Interfaces.Identity;
+using PlaylistApi.Application.Interfaces.Repositories;
+using PlaylistApi.Infrastructure.Authentication;
+using PlaylistApi.Infrastructure.ExternalServices.iTunes;
+using PlaylistApi.Infrastructure.Identity;
+using PlaylistApi.Infrastructure.Persistence;
+using PlaylistApi.Infrastructure.Persistence.Repositories;
+using System.Text;
+
+namespace PlaylistApi.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentityCore<ApplicationUser>(options =>
+            {
+                options.User.RequireUniqueEmail = true;
+
+                options.Password.RequiredLength = 8;
+                options.Password.RequireDigit = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireNonAlphanumeric = true;
+
+            }).AddEntityFrameworkStores<AppDbContext>();
+
+            services.AddScoped<IIdentityService, IdentityService>();
+
+            var jwtKey = configuration["Jwt:Key"]!;
+            var jwtIssuer = configuration["Jwt:Issuer"]!;
+            var jwtAudience = configuration["Jwt:Audience"]!;
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+
+                    ValidIssuer = jwtIssuer,
+                    ValidAudience = jwtAudience,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey))
+                };
+            });
+
+            services.AddScoped<IJwtTokenService, JwtTokenService>();
+
+            services.AddHttpContextAccessor();
+
+            services.AddScoped<ICurrentUserService, CurrentUserService>();
+
+            services.AddScoped<IPlaylistRepository, PlaylistRepository>();
+
+            services.AddScoped<ISongRepository, SongRepository>();
+
+            services.AddHttpClient<IMusicService, ITunesMusicService>(client =>
+            {
+                client.BaseAddress = new Uri("https://itunes.apple.com/");
+            });
+
+            return services;
+        }
+    }
+}
